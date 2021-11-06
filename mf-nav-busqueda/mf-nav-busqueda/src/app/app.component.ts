4396ecdb311fa2c7d519a1c1683f580f53091547
Tomás <tomas.seguram@gmail.com>
2021-11-06 15:13:18 -0600

    mf

---
+++ mf-nav-busqueda/mf-nav-busqueda/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mf-nav-busqueda',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mf-nav-busqueda';
+}
