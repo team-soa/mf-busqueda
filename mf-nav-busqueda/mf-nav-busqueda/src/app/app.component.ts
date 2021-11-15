1aeeabe9100be4c7593cebcdae3f081b7b9cc636
JUARSO <juan09fut.js@gmail.com>
2021-11-14 18:10:25 -0600

    Nav funcional

--- mf-nav-busqueda/mf-nav-busqueda/src/app/app.component.ts
+++ mf-nav-busqueda/mf-nav-busqueda/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mf-nav-busqueda',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
+  
+  constructor(private router: Router){}
+  
+  
+
+  public navigateTo(direccionDeBusqueda: string): void {
+    if(direccionDeBusqueda === 'Login'){
+      this.router.navigateByUrl("Login")
+    }
+    if(direccionDeBusqueda === 'VistaPrincipal'){
+      this.router.navigateByUrl("VistaPrincipal")
+    }
+    if(direccionDeBusqueda === 'VistaPremium'){
+      this.router.navigateByUrl("VistaPremium")
+    }
+
+  }
+
   title = 'mf-nav-busqueda';
 }
+
+
